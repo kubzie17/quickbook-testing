623a36f55169c936d83705aaf03f695844c1422f
Banzi Kubheka <mckubzie@btinternet.com>
2026-04-27 12:27:04 +0100

    new homepage changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div>
       <h2>Welcome to QuickBook</h2>
       <p>Book and manage appointments easily.</p>
+      <p>This is the homepage</p>
     </div>
   );
 }
